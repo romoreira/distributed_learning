d894e4b8a4ce183daf9df58810c0e2a26be73f33
rodrigo <rodrigo.moreira206@gmail.com>
2021-11-08 14:44:12 -0300

    Ready to be distributed.

--- tensor.py
+++ tensor.py
@@ -64,3 +64,36 @@ federated_train_data = make_federated_data(emnist_train, sample_clients)
 
 print('Number of client datasets: {l}'.format(l=len(federated_train_data)))
 print('First dataset: {d}'.format(d=federated_train_data[0]))
+
+#Creating keras model - returns a constructor containing minimal CNN structure.
+def create_keras_model():
+  return tf.keras.models.Sequential([
+      tf.keras.layers.InputLayer(input_shape=(784,)),
+      tf.keras.layers.Dense(10, kernel_initializer='zeros'),
+      tf.keras.layers.Softmax(),
+  ])
+
+def model_fn():
+  # We _must_ create a new model here, and _not_ capture it from an external
+  # scope. TFF will call this within different graph contexts.
+  keras_model = create_keras_model()
+  return tff.learning.from_keras_model(
+      keras_model,
+      input_spec=preprocessed_example_dataset.element_spec,
+      loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+      metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
+
+iterative_process = tff.learning.build_federated_averaging_process(
+    model_fn,
+    client_optimizer_fn=lambda: tf.keras.optimizers.SGD(learning_rate=0.02),
+    server_optimizer_fn=lambda: tf.keras.optimizers.SGD(learning_rate=1.0))
+
+print("type_signature: "+str(iterative_process.initialize.type_signature))
+state = iterative_process.initialize()
+print(state)
+
+NUM_ROUNDS = 11
+for round_num in range(0, NUM_ROUNDS):
+  state, metrics = iterative_process.next(state, federated_train_data)
+  print("state: "+str(state))
+  print('round {:1d}, metrics={}'.format(round_num, metrics))
